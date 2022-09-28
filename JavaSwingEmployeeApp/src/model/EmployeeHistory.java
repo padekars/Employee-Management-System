4a77d18e920f063430a7d17cedb33a692b2c17a2
padekars <padekar.s@northeastern.edu>
2022-09-28 02:08:25 -0400

    Created initial structure and employee form

---
+++ JavaSwingEmployeeApp/src/model/EmployeeHistory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author snehal
+ */
+public class EmployeeHistory {
+    private ArrayList<Employees> history;
+    
+    public EmployeeHistory()
+    {
+        this.history = new ArrayList<Employees>();
+        
+    }
+
+    public ArrayList<Employees> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Employees> history) {
+        this.history = history;
+    }
+    
+}
